58909a41b7c4ef11da3f772aa35434836cf05862
Devang-sharma609 <devang122indmp@gmail.com>
2025-09-26 07:59:38 +0530

    DTO enable annotation added

--- src/main/java/com/devang/abhyudaya/AbhyudayaApplication.java
+++ src/main/java/com/devang/abhyudaya/AbhyudayaApplication.java
@@ -4,11 +4,13 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class AbhyudayaApplication {
 
 	public static void main(String[] args) {
